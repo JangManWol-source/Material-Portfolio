8e790131ed220311a208139996f776495ee0fb58
Joe Cristian Jamis <85830010+JangManWol-source@users.noreply.github.com>
2022-06-08 23:37:09 +0800

    deploy

--- src/components/OverView/OverView.jsx
+++ src/components/OverView/OverView.jsx
@@ -14,9 +14,9 @@ const OverView = () => {
             <div className="title">Quick Overview</div>
             <div className="details"><div><img src={graduate} alt="graduate" /></div><div>I am Computer Science Student at ICCT</div></div>
             <div className="details"><div><img src={award} alt="award" /></div><div>I finished senior high school with honor at MaNHS.</div></div>
-            <div className="details"><div><img src={code} alt="" /></div><div>I am the developer of our thesis app.</div></div>
-            <div className="details"><div><img src={work} alt="" /></div><div>Having part time job and free lancing at Linkedin and another platform to gain experience.</div></div>
-            <div className="details"><div><img src={react} alt="" /></div><div>I love React.js library.</div></div>
+            <div className="details"><div><img src={code} alt="code" /></div><div>I am the developer of our thesis app.</div></div>
+            <div className="details"><div><img src={work} alt="work" /></div><div>Having part time job and free lancing at Linkedin and another platform to gain experience.</div></div>
+            <div className="details"><div><img src={react} alt="react" /></div><div>I love React.js library.</div></div>
             <div className='title' style={{textAlign:'left', marginTop:10}}>Recent Study</div>
             <div className='study'><img src={study} alt="" /></div>
         </Card>
